fd9e2772374be1387e0f392295db3882d923f3a0
Guilherme Tosta <guilherme_tosta@hotmail.com>
2020-10-28 00:31:05 -0300

    aula3 28/10

---
+++ estudo-heroway/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header" width = "20px">
+        <button onclick="" class="btnMais">
+         +
+        </button>
+        Contador: 0
+        <button class="btnMenos">
+         -
+        </button>
+        
+  
+      </header>
+    </div>
+  );
+}
+
+export default App;
